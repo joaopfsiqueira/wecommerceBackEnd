714f1d57d8933c4afaff83cf4d682af39926c556
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-07-29 18:12:35 -0300

    feat: importing data on express

---
+++ data.js
@@ -0,0 +1,56 @@
+const data = {
+    products : [
+        {
+            name: 'Call of Duty Cold War PS5',
+            slug: 'cod-cold-war-ps5',
+            category: 'PS5',
+            image: '/images/cod-cold-war-ps5.jpg',
+            price: 250,
+            countIStock: 10,
+            brand: 'Activision',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Call of Duty Cold War PS5, 2021 game of the year',
+        },
+        {
+            name: 'Call of duty Vanguard PS5',
+            slug: 'cod-vanguard-ps5',
+            category: 'PS5',
+            image: '/images/cod-vanguard-ps5.jpg',
+            price: 350,
+            countIStock: 20,
+            brand: 'Activision',
+            rating: 3,
+            numReviews: 10,
+            description: 'Call of Duty Vanguard PS5, 2022 game',
+
+        },
+        {
+            name: 'Maneater PS5',
+            slug: 'maneater-ps5',
+            category: 'PS5',
+            image: '/images/man-eater-ps5.jpg',
+            price: 150,
+            countIStock: 4,
+            brand: 'Deep Silver',
+            rating: 2,
+            rumReviews: 3,
+            description: 'Maneater PS5, 2022 game',
+        },
+        {
+            name: 'PS5',
+            slug: 'ps5',
+            category: 'PS5',
+            image: '/images/ps5.jpg',
+            price: 5000,
+            countIStock: 1,
+            brand: 'Sony',
+            rating: 5,
+            numReviews: 10,
+            description: 'PS5, console complete with one controller and 3 games',
+        }
+    ],
+};
+
+
+module.exports = data;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import data from "./data"
 
 const app = express();
 
 app.get('/api/produts', (req, res) => {
-  res.send('Hello World!');
+  res.send(data);
 });
